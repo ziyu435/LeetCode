9e7c06d769bf04633a4074305cae60acdce331cc
ziyu435 <60421530+ziyu435@users.noreply.github.com>
2020-04-08 22:37:31 +0800

    Create 933NumberRecentCalls.java

---
+++ Queue/933NumberRecentCalls.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RecentCounter {
+    Queue<Integer> q;
+
+    public RecentCounter() {
+        q = new LinkedList<Integer>();
+    }
+
+    public int ping(int t) {
+        q.offer(t);
+        while(q.peek() < t - 3000){
+            q.poll();
+        }
+        return q.size();
+    }
+}
