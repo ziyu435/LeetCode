26abf932831f28ad88f9ce0aaec3569f6b999377
ziyu435 <60421530+ziyu435@users.noreply.github.com>
2020-04-27 09:39:22 +0800

    Create 700SearchInABinarySearchTree.java

---
+++ 700SearchInABinarySearchTree.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        // recursion terminal condition
+        if(root == null || root.val == val) {
+            return root;
+        }
+        return root.val < val ? searchBST(root.right, val) : searchBST(root.left, val);
+    }
+}
