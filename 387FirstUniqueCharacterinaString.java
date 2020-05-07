2d7b84e73fdf0c8fcb5382942ad0694898673e75
ziyu435 <60421530+ziyu435@users.noreply.github.com>
2020-05-07 21:32:55 +0800

    Create 387FirstUniqueCharacterinaString.java

---
+++ 387FirstUniqueCharacterinaString.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class Solution {
+    public int firstUniqChar(String s) {
+        // Built the hash map to count the character frequency
+        HashMap<Character, Integer> count = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            count.put(s.charAt(i), count.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for(int i = 0; i < s.length(); i++) {
+            if(count.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
